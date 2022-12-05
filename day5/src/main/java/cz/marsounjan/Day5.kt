4209ee46f664adf300beb4189b81d22200a3be85
Jan Maršoun <marsounjan@gmail.com>
2022-12-05 23:14:26 +0100

    day 5 part one

--- day5/src/main/java/cz/marsounjan/Day5.kt
+++ day5/src/main/java/cz/marsounjan/Day5.kt
@@ -10,17 +10,49 @@ class Day5 {
         puzzleInput.readBytes().toString(UTF_8)
     }
 
-    private val elvenRanges by lazy {
-        puzzleInputString.lines()
-            .map { it.split(",")
-                .map { it.split("-") }
-                .map { it.map { it.toInt() } }
-                .map { it[0] .. it[1] }
+    private val cratesInput = File("./day5/cratesInput.txt")
+    private val cratesInputString by lazy {
+        cratesInput.readBytes().toString(UTF_8)
+    }
+
+    private val moveRegex = "move (\\d+) from (\\d+) to (\\d+)".toRegex()
+
+    fun crates(): MutableList<MutableList<String>> = mutableListOf<MutableList<String>>().apply {
+        cratesInputString.lines()
+            .reversed()
+            .forEachIndexed { lineIndex, line ->
+                line.chunked(4)
+                    .map { it.trim().removePrefix("[").removeSuffix("]") }
+                    .forEachIndexed { crateIndex, crate ->
+                        if (crate.isNotBlank()) {
+                            println("Crate $crate at $lineIndex:$crateIndex")
+                            if (lineIndex == 0) add(mutableListOf(crate)) else this.get(crateIndex).add(crate)
+                        } else {
+                            println("No crate at $lineIndex:$crateIndex")
+                        }
+                    }
             }
     }
 
-    fun partOne(): Int {
-        return 0
+    private fun moves() : List<Triple<Int, Int, Int>> = puzzleInputString.lines()
+            .map { line ->
+                val matchResult = moveRegex.find(line)!!
+                Triple(
+                    matchResult.groups[1]!!.value.toInt(),
+                    matchResult.groups[2]!!.value.toInt(),
+                    matchResult.groups[3]!!.value.toInt()
+                )
+            }
+
+    fun partOne(): String {
+        val crates = crates()
+        val moves = moves()
+        moves.forEach { (amount, from, to) ->
+            (0 until amount).forEach {
+                crates[to-1].add(crates[from-1].removeLast())
+            }
+        }
+        return crates.map { it.last() }.joinToString(separator = "")
     }
 
     fun partTwo(): Int {
