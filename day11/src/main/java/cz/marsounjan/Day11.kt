7fa2c795e552f2291629079ae852aa8e83fde540
Jan Maršoun <marsounjan@gmail.com>
2022-12-11 15:52:56 +0100

    day 11 part start

---
+++ day11/src/main/java/cz/marsounjan/Day11.kt
@@ -0,0 +1,27 @@
+package cz.marsounjan
+
+import java.io.File
+import java.nio.charset.StandardCharsets.UTF_8
+
+class Day11 {
+
+    private val puzzleInput = File("./day11/input.txt")
+    private val puzzleInputString by lazy {
+        puzzleInput.readBytes().toString(UTF_8)
+    }
+
+    fun partOne(): Int {
+        return 0
+    }
+
+    fun partTwo(): Int {
+       return 0
+    }
+
+}
+
+fun main(args: Array<String>) {
+    val puzzle = Day11()
+    println("Puzzle output Part 1: ${puzzle.partOne()}")
+    println("Puzzle output Part 2: ${puzzle.partTwo()}")
+}
